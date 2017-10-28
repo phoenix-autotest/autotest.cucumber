c6b56eae500f4f0e5d4ec3c678987d59ac715ba9
surenpi <zxjlwt@126.com>
2017-10-28 23:10:26 +0800

    增加基本的操作封装

--- src/main/java/com/surenpi/autotest/cucumber/ElementStep.java
+++ src/main/java/com/surenpi/autotest/cucumber/ElementStep.java
@@ -18,10 +18,39 @@ public class ElementStep
     private SeleniumEngine engine;
     private WebElement webElement;
 
-    @当("^根据标签ID查找元素(\\w+)$")
+    @当("^根据标签的属性id查找元素(\\w+)$")
     public void findById(String id)
     {
-        webElement = engine.getDriver().findElement(By.id(id));
+        find(By.id(id));
+    }
+
+    @当("^根据标签的属性name查找元素(\\w+)$")
+    public void findByName(String name)
+    {
+        find(By.name(name));
+    }
+
+    @当("^根据标签名称查找元素(\\w+)$")
+    public void findByTagName(String tagName)
+    {
+        find(By.tagName(tagName));
+    }
+
+    @当("^根据超链接文本查找元素(\\w+)$")
+    public void findByLinkText(String linkText)
+    {
+        find(By.linkText(linkText));
+    }
+
+    @当("^根据超链接文本模糊查找元素(\\w+)$")
+    public void findByPartialLinkText(String partialLinkText)
+    {
+        find(By.partialLinkText(partialLinkText));
+    }
+
+    private void find(By by)
+    {
+        webElement = engine.getDriver().findElement(by);
     }
 
     @那么("^输入(\\w+)$")
